6f7f9912bcfaf3ecb618f50fbc45c2e3df1ff912
IamReefShark <zhijianliu@pm.me>
2023-08-29 08:46:25 +0800

    d

--- test822.py
+++ test822.py
@@ -1,4 +1,4 @@
-# from statsmodels.stats.proportion import proportions
+# from statsmodels.stats.proportion import proportion
 # import numpy as np
 # print(ztest(count=np.array([13736,17210]), nobs=np.array([59840,60063])))
 #
