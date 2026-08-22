a7c0078d2e69ee3694086cad6ac429e13bfddc7d
vvvgaming <zhijianliu@pm.me>
2026-08-22 10:00:00 +0800

    d

--- 55.py
+++ 55.py
@@ -1,6 +1,6 @@
 # Z2=(pb-pa)/((pa*(1-pa)/na)+(pb*(1-pb)/nb))**0.5
 # print(Z2)
-# p2=norm.sf(abs(z_scores))*2 #twosi
+# p2=norm.sf(abs(z_scores))*2 #twos
 
 # Z2=(pb-pa)/((pa*(1-pa)/na)+(pb*(1-pb)/nb))**0.5
 # print(Z2)
